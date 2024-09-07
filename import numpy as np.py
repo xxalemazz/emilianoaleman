91b9119517b758a97847b9e4a92a74e7f5da18f2
Emiliano <emiliano@gmail.com>
2024-09-07 01:20:02 -0600

    Agregue import numpy as pd.py

---
+++ import numpy as np.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+# Crear un arreglo 1D
+arr_1d = np.array([1, 2, 3])
+print("Arreglo 1D:", arr_1d)
+
+# Crear un arreglo 2D
+arr_2d = np.array([[1, 2], [3, 4]])
+print("Arreglo 2D:\n", arr_2d)
+
+# Sumar todos los elementos del arreglo 2D
+suma_total = np.sum(arr_2d)
+print("Suma total de elementos en arr_2d:", suma_total)
